fdea7763269c0f9ebd0ce0ecf14db444fe9a50fb
Chandan Kumar <descentchandan@gmail.com>
2020-10-01 15:12:13 +0530

    try merging array

--- reverseString.js
+++ reverseString.js
@@ -1,5 +1,26 @@
-function rev(str) {
-    console.log(str.split("").reverse().join(""))
+function mergeShort(arr1,arr2) {
+    let mergedArray=[]
+    arr1.forEach(e=> {
+        mergedArray.push(e)
+    });
+
+    arr2.forEach(e=> {
+        mergedArray.push(e)
+    });
+    
+    console.log(mergedArray);
+
+    let sortedArray=[]
+    
+    for(let i=0; i<mergedArray.length;i++){
+        if (mergedArray[i] < mergedArray[i + 1]) {
+          sortedArray.push(mergedArray[i]);
+        } else {
+          sortedArray.push(mergedArray[i + 1]);
+        }
+    }
+
+    return sortedArray
 }
 
-rev("hello")
+console.log(mergeShort([0, 3, 4, 31], [4, 6, 30]));
