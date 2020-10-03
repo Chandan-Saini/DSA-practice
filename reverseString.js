950132948d58490ab211c7c0fc365bc4507ce28d
Chandan Kumar <descentchandan@gmail.com>
2020-10-03 15:28:46 +0530

    tried two sum leet code

--- reverseString.js
+++ reverseString.js
@@ -1,26 +1,9 @@
-function mergeShort(arr1,arr2) {
-    let mergedArray=[]
-    arr1.forEach(e=> {
-        mergedArray.push(e)
-    });
-
-    arr2.forEach(e=> {
-        mergedArray.push(e)
-    });
-    
-    console.log(mergedArray);
-
-    let sortedArray=[]
-    
-    for(let i=0; i<mergedArray.length;i++){
-        if (mergedArray[i] < mergedArray[i + 1]) {
-          sortedArray.push(mergedArray[i]);
-        } else {
-          sortedArray.push(mergedArray[i + 1]);
-        }
+var twoSum = function (nums, target) {
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] + nums[i + 1] === target) {
+      return [i, i + 1];
     }
+  }
+};
 
-    return sortedArray
-}
-
-console.log(mergeShort([0, 3, 4, 31], [4, 6, 30]));
+// tried two sum leet code
