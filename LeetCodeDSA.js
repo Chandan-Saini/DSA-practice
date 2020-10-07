80739e6f244b1f2de6ae7a5f2bf9a1465fd469eb
Chandan Kumar <descentchandan@gmail.com>
2020-10-07 22:58:41 +0530

    file name changed

--- LeetCodeDSA.js
+++ LeetCodeDSA.js
@@ -1,9 +1,12 @@
 var rotate = function (nums, k) {
   var arr = [];
-  for (var i = 0; i < nums; i++) {
+  for (var i = 0; i < k; i++) {
     arr.push(nums[i]);
     nums[i] = nums[nums.length - 1];
   }
 
   return arr.concat(nums);
 };
+
+
+console.log(rotate([1,2,3,4,5,6,7],3))
